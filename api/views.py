aa101631abbe49f363d722dd5cae3a91972edf1b
AzaanMahmood <azaanmahmood007@gmail.com>
2024-04-16 16:25:54 +0500

    Add Pagination

--- api/views.py
+++ api/views.py
@@ -1,28 +1,32 @@
 from django.shortcuts import render
-from .serializers import  BookSerializer
+from .serializers import  BookSerializer, ActiveBooksByAuthorSerializer
 from .models import Book
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin,CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 
+from .filter import IsActiveFilter
+
 
 class BookListCreate(GenericAPIView, ListModelMixin, CreateModelMixin):
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
-
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['author']
+    filterset_class = IsActiveFilter  # Use the custom filter class here
+
 
     def get_queryset(self):
         return Book.objects.fil()
-    
 
-    def get(self, request, *arg, **kwargs):
-        return self.list(request, *arg, **kwargs)
-    
-    def post(self, request, *arg, **kwargs):
-        return self.create(request, *arg, **kwargs)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
 
 class BookRUD(GenericAPIView, UpdateModelMixin, RetrieveModelMixin, DestroyModelMixin):
     queryset = Book.objects.all()
@@ -37,3 +41,15 @@ class BookRUD(GenericAPIView, UpdateModelMixin, RetrieveModelMixin, DestroyModel
 
     def delete(self, request, *arg, **kwargs):
         return self.destroy(request, *arg, **kwargs)
+
+
+
+class ActiveBooksByAuthor(GenericAPIView, ListModelMixin):
+    serializer_class = ActiveBooksByAuthorSerializer
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        author_id = self.kwargs['author_id']
+        return Book.objects.filter(author_id=author_id, is_active=True)
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
