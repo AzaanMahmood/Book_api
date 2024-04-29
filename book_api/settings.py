9fd78d2eae2b9a0b806ec49371d2f2cb9bae3a5f
AzaanMahmood <azaanmahmood007@gmail.com>
2024-04-29 13:11:29 +0500

    implement Redis Chache and Management Command

--- book_api/settings.py
+++ book_api/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django_filters',
     'api',
     'user',
+    'shop_stats',
 ]
 
 MIDDLEWARE = [
@@ -151,3 +152,14 @@ REST_FRAMEWORK = {
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        "LOCATION": "redis://127.0.0.1:6379",
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
--- user/views.py
+++ user/views.py
@@ -5,14 +5,21 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin,CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
-
+from django.core.cache import cache
+from django.conf import settings
 
 class AuthorListCreate(GenericAPIView, ListModelMixin, CreateModelMixin):
     serializer_class = AuthorSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Author.objects.filter(created_by=self.request.user, is_active=True)
+        cached_queryset = cache.get('author_list_queryset')
+        if cached_queryset:
+            return cached_queryset
+
+        queryset = Author.objects.all()
+        cache.set('author_list_queryset', queryset, timeout=3600) 
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
@@ -29,10 +36,12 @@ class AuthorRUD(GenericAPIView, UpdateModelMixin, RetrieveModelMixin, DestroyMod
     permission_classes = [IsAuthenticated]
 
     def get_object(self):
+        cached_object = cache.get(f'author_{self.kwargs["pk"]}')
+        if cached_object:
+            return cached_object
+
         obj = super().get_object()
-        user = self.request.user
-        if obj.created_by != user:
-            raise PermissionDenied("You do not have permission to access this object.")
+        cache.set(f'author_{obj.pk}', obj, timeout=3600)  
         return obj
 
     def get_queryset(self):
